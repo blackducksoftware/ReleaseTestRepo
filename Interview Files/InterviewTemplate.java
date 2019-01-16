3f1e3850b0e4d9250b506e86b9a733f1c3033aec
jrichard <jrichard@synopsys.com>
2019-01-16 16:23:01 -0500

    Updating the Interview files

---
+++ Interview Files/InterviewTemplate.java
@@ -0,0 +1,17 @@
+// some suggested imports, but you are free to choose any classes from the JDK
+
+import java.lang.Character;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.Reader;
+import java.io.InputStreamReader;
+
+import java.nio.charset.StandardCharsets;
+
+public class Interview {
+    public static void main(String[] args) throws Exception {
+    }
+
+}
